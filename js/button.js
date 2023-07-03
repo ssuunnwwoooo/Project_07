b695b675f4bd4015bcfc0a6e922d9b6a6f088ad1
ssuunnwwoooo <o3olia@naver.com>
2023-07-03 18:17:56 +0900

    230703

---
+++ js/button.js
@@ -0,0 +1,14 @@
+$(function () {
+    $('.main_slide').slick({
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
+
+    $('.game_slide').slick({
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
+
+});
